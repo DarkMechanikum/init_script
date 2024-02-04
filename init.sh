968e61753af30a83b02c7c5b6ece469714477d92
DarkMechanikum <v.shtarev08@gmail.com>
2024-02-04 21:26:09 +0200

    v0.0

---
+++ init.sh
@@ -0,0 +1,6 @@
+apt update
+apt upgrade
+apt purge firefox -y
+apt autoremove
+apt install -y ca-certificates curl dash diffuils dkms easy-rsa freecad gimp git gparted grep gzip htop micro openssh-server openvpn snapd thunderbird tmux wget wireshark firefox
+apt autoremove 

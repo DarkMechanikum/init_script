50255e71fa9df548d5e98fe8a1535aded8c20c10
DarkMechanikum <v.shtarev08@gmail.com>
2024-02-05 09:45:08 +0200

    v0.01

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@ apt update
 apt upgrade
 apt purge firefox -y
 apt autoremove
-apt install -y ca-certificates curl dash diffuils dkms easy-rsa freecad gimp git gparted grep gzip htop micro openssh-server openvpn snapd thunderbird tmux wget wireshark firefox
+apt install -y ca-certificates curl dash diffuils dkms easy-rsa freecad gimp git gparted grep gzip htop micro openssh-server openvpn snapd thunderbird tmux wget wireshark firefox zip unzip
 apt autoremove 
+wget https://github.com/balena-io/etcher/releases/download/v1.18.11/balenaEtcher-1.18.11-x64.AppImage
